323df9b2bd366397db4a8416335b8c33bcb3ddbb
Krister Wolfhard <krister.wolfhard@it-designers.de>
2020-05-03 16:31:11 +0200

    Remove prints from quick sort

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod bubble_sort;
 mod random_sort;
+mod quick_sort;
 
 
 fn main() {
--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -16,27 +16,21 @@ fn _partition(arr: &mut [i32], lo: isize, hi: isize) -> isize {
             break;
         } else {
             arr.swap(i as usize, j as usize);
-            println!("1{:?}", arr);
         }
     }
     arr.swap(i as usize, pivot as usize);
-    println!("2{:?}", arr);
     i
 }
 
 fn _quick_sort(arr: &mut [i32], lo: isize, hi: isize) {
     if lo < hi {
         let p = _partition(arr, lo, hi);
-        println!("3{:?}", arr);
         _quick_sort(arr, lo, p - 1);
-        println!("4{:?}", arr);
         _quick_sort(arr, p + 1, hi);
-        println!("5{:?}", arr);
     }
 }
 
 pub fn quick_sort(arr: &mut [i32]) {
     let len = arr.len();
     _quick_sort(arr, 0, (len - 1) as isize);
-    println!("6{:?}", arr);
 }
