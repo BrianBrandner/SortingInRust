24f9c8e4ed8eb51a2826907155207a564e31528c
Krister Wolfhard <krister.wolfhard@it-designers.de>
2020-05-03 17:39:25 +0200

    Add selection sort

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod quick_sort;
 mod counting_sort;
 mod heap_sort;
 mod merge_sort;
+mod selection_sort;
 
 
 fn main() {
---
+++ src/selection_sort.rs
@@ -0,0 +1,12 @@
+pub fn selection_sort(arr: &mut [i32]) {
+    let len = arr.len();
+    for left in 0..len {
+        let mut smallest = left;
+        for right in (left + 1)..len {
+            if arr[right] < arr[smallest] {
+                smallest = right;
+            }
+        }
+        arr.swap(smallest, left);
+    }
+}
