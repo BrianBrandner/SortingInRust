f8d3c9cb155ea4192679ac311a40b082776abf83
Krister Wolfhard <krister.wolfhard@it-designers.de>
2020-05-04 20:30:24 +0200

    Change Vector values

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,14 @@ fn main() {
 
 
 fn create_shuffled_vector(length: i32) -> Vec<i32> {
-    let mut vec: Vec<i32> = (0..length).collect();
+    let mut vec: Vec<i32> = (1..length+1).collect();
     vec.shuffle(&mut thread_rng());
     vec
 }
 
 
 fn create_reversed_vector(length: i32) -> Vec<i32> {
-    let mut vec: Vec<i32> = (0..length).collect();
+    let mut vec: Vec<i32> = (1..length+1).collect();
     vec.reverse();
     vec
 }
