888633ee4d99da2b3b696d0934f0937057597d9a
Brian Brander <ceinaxt@posteo.de>
2020-05-05 20:01:55 +0200

    good output

--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -1,8 +1,10 @@
-pub fn bubble_sort(array: &mut [i32]) {
+
+pub fn bubble_sort(array: & mut Vec<u32>, steps: &mut Vec<Vec<u32>>) {
     for i in 0..array.len() {
         for j in 0..array.len() - 1 - i {
             if array[j] > array[j + 1] {
                 array.swap(j, j + 1);
+                steps.push(array.clone())
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #[macro_use]
+#[warn(unused_imports)]
 extern crate stdweb;
 
 use crate::canvas::Canvas;
-use std::borrow::Borrow;
-use stdweb::web::set_timeout;
+use std::borrow::{Borrow, BorrowMut};
+use bubble_sort::bubble_sort;
 
 
 mod bubble_sort;
@@ -18,7 +19,7 @@ mod canvas;
 
 use rand::thread_rng;
 use rand::seq::SliceRandom;
-
+use stdweb::web::set_timeout;
 
 fn main() {
     stdweb::initialize();
@@ -28,26 +29,34 @@ fn main() {
 
     }
 
-    let array = create_shuffled_vector(25);
-    let array2 = create_reversed_vector(14);
+    let mut array = create_reversed_vector(14);
     let max = array.iter().max().unwrap().clone();
     let canvas = Canvas::new("canvas", array.len() as u32, max + 5);
+    let mut sorting_steps: Vec<Vec<u32>> = vec![array.clone()];
+    bubble_sort(array.borrow_mut(), sorting_steps.borrow_mut());
+    draw_step(canvas, sorting_steps);
+    stdweb::event_loop();
+}
 
-    draw_canvas(canvas.borrow());
-    draw_array(canvas.borrow(), array);
+//pub fn draw_update(canvas: &Canvas, array: &Vec<u32>){
+//    set_timeout(move ||{
+//        draw_array(canvas.borrow(),array.borrow())
+//    }, 1000);
+//}
 
+fn draw_step(canvas: Canvas, mut steps:  Vec<Vec<u32>>){
     set_timeout(move ||{
-        draw_canvas(canvas.borrow());
-        draw_array(canvas.borrow(), array2);
-        stdweb::event_loop();
-    },3000);
+        if !steps.is_empty() {
+            let mut remaining_steps = steps.split_off(1);
+            draw_array(&canvas, &steps[0]);
+            draw_step(canvas, remaining_steps);
+        }
+    }, 100);
 }
 
-fn draw_canvas(canvas: &Canvas) {
+
+fn draw_array(canvas: &Canvas, array: &Vec<u32>) {
     canvas.set_canvase_color("grey");
-}
-
-fn draw_array(canvas: &Canvas, array: Vec<u32>) {
     let mut i: i32 = 0;
     while i < array.len() as i32 {
         draw_column(canvas.borrow(), array[i as usize] as u32, i as u32);
@@ -62,7 +71,6 @@ fn draw_column(canvas: &Canvas, height: u32, position: u32) {
         i = i -1;
     }
 }
-
 
 fn create_shuffled_vector(length: u32) -> Vec<u32> {
     let mut vec: Vec<u32> = (1..length+1).collect();
