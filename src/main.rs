c9715756c8ff48d2a6c9dadeebee8a0d8515ba5a
Brian Brander <ceinaxt@posteo.de>
2020-05-07 14:08:52 +0200

    better makeing

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate  instant;
 
 use crate::canvas::Canvas;
 use std::borrow::{Borrow, BorrowMut};
-use bubble_sort::bubble_sort;
+use bubble_sort::BubbleSort;
 
 mod bubble_sort;
 mod random_sort;
@@ -20,38 +20,84 @@ mod canvas;
 
 use rand::thread_rng;
 use rand::seq::SliceRandom;
-use stdweb::web::{set_timeout, document, INonElementParentNode, Element, IParentNode};
+use stdweb::web::{set_timeout, document, INonElementParentNode, Element, IParentNode, IEventTarget, IElement};
 use instant::{Instant};
+use stdweb::web::event::{ClickEvent, IEvent, InputEvent};
+use std::rc::Rc;
+use std::sync::{RwLock, Mutex};
 
+trait SortingAlg {
+    fn sort(&self,array: & mut Vec<u32>, steps: &mut Vec<Vec<u32>>);
+}
+
+struct SortingProcess {
+    canvas: Canvas,
+    delay: Rc<RwLock<u32>>,
+    length: u32,
+    sorting_steps: Vec<Vec<u32>>,
+    abort: bool,
+}
 
 fn main() {
     stdweb::initialize();
-
-    let mut array = create_shuffled_vector(25);
-    let max = array.iter().max().unwrap().clone();
-    let canvas = Canvas::new("canvas", array.len() as u32, max + 5);
-    let mut sorting_steps: Vec<Vec<u32>> = vec![array.clone()];
-    let start = Instant::now();
-    bubble_sort(array.borrow_mut(), sorting_steps.borrow_mut());
-    let duration = start.elapsed().as_micros() as i32;
-    let some_element = document().query_selector("#elapsed_time").unwrap().unwrap();
-
-    js!{
-    @{some_element}.innerHTML = @{duration};
+    let length :u32 = document().get_element_by_id("size").unwrap().get_attribute("value").unwrap().parse().expect("not an Number");
+    let slider_time = document().query_selector( "#millsec" ).unwrap().unwrap();
+    let mut delay: Rc<RwLock<u32>> = {
+        let attr_value = slider_time.get_attribute("value").unwrap();
+        Rc::new(  RwLock::new(attr_value.parse().expect("not an Number")))
     };
-
-    draw_step(canvas, sorting_steps);
+    let temp_rc = delay.clone();
+    slider_time.clone().add_event_listener( move |current_delay: InputEvent| {
+        let attr_value = slider_time.get_attribute("value").unwrap();
+        *temp_rc.write().unwrap() = attr_value.parse().expect("not an Number");
+    });
+    let mut current_process: Option<Rc<RwLock<SortingProcess>>> = None;
+    let button = document().query_selector( "#start" ).unwrap().unwrap();
+    button.add_event_listener( move | sorting: ClickEvent| {
+        if let Some(process) = &current_process{
+            process.write().unwrap().abort = true;
+        }
+        current_process = Some(start_sorting(length,delay.clone(), &BubbleSort));
+    });
     stdweb::event_loop();
 }
 
-fn draw_step(canvas: Canvas, mut steps:  Vec<Vec<u32>>){
+fn start_sorting<S: SortingAlg>(length: u32, delay : Rc<RwLock<u32>>, sorting_alg: &S) -> Rc<RwLock<SortingProcess>> {
+    let mut array = create_shuffled_vector(length);
+    let canvas = Canvas::new("canvas", length, length + 5);
+    let mut sorting_steps: Vec<Vec<u32>> = vec![array.clone()];
+    let start = Instant::now();
+    sorting_alg.sort(&mut array, &mut sorting_steps);
+    let duration = start.elapsed().as_millis() as i32;
+    let time_element = document().query_selector("#elapsed_time").unwrap().unwrap();
+    js!{
+    @{time_element}.innerHTML = @{duration};
+    };
+    let sorting_process = Rc::new(RwLock::new(SortingProcess{
+        canvas,
+        delay,
+        length,sorting_steps,
+        abort: false,
+    }));
+    draw_step(sorting_process.clone());
+    sorting_process
+}
+
+fn draw_step(sorting_process: Rc<RwLock<SortingProcess>>){
+    if sorting_process.read().unwrap().abort {
+        return;
+    }
+    let delay = *sorting_process.read().unwrap().delay.read().unwrap();
     set_timeout(move ||{
-        if !steps.is_empty() {
-            let mut remaining_steps = steps.split_off(1);
-            draw_array(&canvas, &steps[0]);
-            draw_step(canvas, remaining_steps);
+        let mut process = sorting_process.write().unwrap();
+        if !process.sorting_steps.is_empty() {
+            let remaining_steps = process.sorting_steps.split_off(1);
+            draw_array(&process.canvas, &process.sorting_steps[0]);
+            process.sorting_steps = remaining_steps;
+            drop(process);
+            draw_step(sorting_process);
         }
-    }, 100);
+    }, delay);
 }
 
 
