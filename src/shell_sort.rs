f54fbf400184abd217d5d104c9d95cfdd7358781
Krister Wolfhard <krister.wolfhard@it-designers.de>
2020-05-08 17:43:51 +0200

    Adjust shell sort

--- src/shell_sort.rs
+++ src/shell_sort.rs
@@ -1,25 +1,32 @@
-pub fn shell_sort(values: &mut [i32]) {
-    // shell sort works by swiping the value at a given gap and decreasing the gap to 1
+use crate::SortingAlg;
 
-    let mut count_sublist = values.len() / 2; // makes gap as long as half of the array
-    while count_sublist > 0 {
-        for pos_start in 0..count_sublist {
-            insertion(values, pos_start, count_sublist);
+pub struct ShellSort;
+
+impl SortingAlg for ShellSort {
+    fn sort(&self, array: &mut Vec<u32>, steps: &mut Vec<Vec<u32>>) {
+        // shell sort works by swiping the value at a given gap and decreasing the gap to 1
+
+        let mut count_sublist = array.len() / 2; // makes gap as long as half of the array
+        while count_sublist > 0 {
+            for pos_start in 0..count_sublist {
+                insertion(array, pos_start, count_sublist, steps);
+            }
+            count_sublist /= 2; // makes gap as half of previous
         }
-        count_sublist /= 2; // makes gap as half of previous
     }
 }
 
 //TODO: Idk what .step_by() should be.. :(
-fn insertion(values: &mut [i32], start: usize, gap: usize) {
-    for i in ((start + gap)..values.len()).step_by(gap) {
-        let val_current = values[i];
+fn insertion(array: &mut Vec<u32>, start: usize, gap: usize, steps: &mut Vec<Vec<u32>>) {
+    for i in ((start + gap)..array.len()).step_by(gap) {
+        let val_current = array[i];
         let mut pos = i;
         // make swaps
-        while pos >= gap && values[pos - gap] > val_current {
-            values[pos] = values[pos - gap];
+        while pos >= gap && array[pos - gap] > val_current {
+            array[pos] = array[pos - gap];
             pos = pos - gap;
+            steps.push(array.clone())
         }
-        values[pos] = val_current;
+        array[pos] = val_current;
     }
 }
